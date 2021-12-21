0fcd23b8d9b1f91c0f5a616be99343a0d5e2c0c9
Philippe Jones <pjones@jstrategia.com>
2021-12-21 11:42:43 -0500

    Add project files.
    feat : added CalendarDatePicker to showcase the bug
    
    chore : moved the date picker at the top
    
    chore : added margin for iOS
    
    chore : removed unused code
    
    chore : removed unused import and unused class

---
+++ CalendarDatePickerBug.Shared/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace CalendarDatePickerBug
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new MainPageViewModel();
+        }
+    }
+}
---
+++ CalendarDatePickerBug.Shared/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CalendarDatePickerBug
+{
+    [Windows.UI.Xaml.Data.Bindable]
+    public partial class MainPageViewModel : INotifyPropertyChanged
+    {
+        private DateTimeOffset? _chosenDate;
+
+        public DateTimeOffset? ChosenDate
+        {
+            get => _chosenDate;
+            set
+            {
+                _chosenDate = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ChosenDate"));
+            }
+        }
+
+        public DateTimeOffset CurrentDate { get; } = DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
